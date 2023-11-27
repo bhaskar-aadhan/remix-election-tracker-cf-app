ab18fed94c4bb9af1699a51ed8ff22a749e606ef
bhaskar.guthula <bhaskar.guthula@aadhan.in>
2023-11-27 16:37:46 +0530

    websocket test recurcve fun added

--- app/services/context/index.jsx
+++ app/services/context/index.jsx
@@ -4,35 +4,39 @@ export const ElectionContext = createContext("aadhan");
 
 export const WebscoketContext = ({ children }) => {
     const [webSocketData, setWebSocketData] = useState(null);
-    const [ stateName, setStateName ] = useState("Telangana")
+    const [stateName, setStateName] = useState("Telangana")
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log("window", window.FocusEvent)
     }, [])
 
     //WEBSOCKET
     useEffect(() => {
         const socket = new WebSocket('wss://stage-cmsapis.aadhan.in/election-results/ws');
-        socket.onopen = () => {
-            console.log('WebSocket connection opened');
-        };
+        const handleWebsocket = () => {
+            socket.onopen = () => {
+                console.log('WebSocket connection opened');
+            };
+            socket.onmessage = (event) => {
+                const wsdata = event.data
+                const wsData = JSON.parse(wsdata)
+                setWebSocketData(wsData)
+                console.log("websocket data: ", wsData, typeof (wsData))
+            };
+            socket.onerror = (error) => {
+                console.error('WebSocket error:', error);
+            };
+            socket.onclose = (event) => {
+                if (event.wasClean) {
+                    console.log(`WebSocket connection closed cleanly, code=${event.code}, reason=${event.reason}`);
+                } else {
+                    console.error('WebSocket connection abruptly closed');
+                }
+                handleWebsocket();
+            };
+        }
+        handleWebsocket();
 
-        socket.onmessage = (event) => {
-            const wsdata = event.data
-            const wsData = JSON.parse(wsdata)
-            setWebSocketData(wsData)
-            console.log("websocket data: ", wsData, typeof (wsData))
-        };
-        socket.onclose = (event) => {
-            if (event.wasClean) {
-                console.log(`WebSocket connection closed cleanly, code=${event.code}, reason=${event.reason}`);
-            } else {
-                console.error('WebSocket connection abruptly closed');
-            }
-        };
-        socket.onerror = (error) => {
-            console.error('WebSocket error:', error);
-        };
         return () => {
             if (socket && socket.readyState === WebSocket.OPEN) {
                 socket.close();
