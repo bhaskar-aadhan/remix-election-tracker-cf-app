750f33fe2a2a59ed9e2acb686f6902d7739cb7bb
bhaskar.guthula <bhaskar.guthula@aadhan.in>
2023-11-29 13:14:08 +0530

    widget ui modified

--- app/routes/widget.jsx
+++ app/routes/widget.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { indiaMapImg, voteImg, vottingBoothImg, chevronright } from '../assets/images';
+import { indiaMapImg, voteImg, vottingBoothImg } from '../assets/images';
 
 const widget = () => {
   return (
-    <div className='bg-[#262C42] h-[200px] relative text-white font__chivo overflow-hidden'>
-      <div className="widget-container flex justify-between items-end">
+    <div className='bg-[#262C42] max-h-[200px] relative text-white font__chivo overflow-hidden'>
+      <div className="widget-container max-h-[167.5px] mb-[32.5px] flex ">
       <div className="widget-container-1 flex-1 p-[0.5rem]">
         <div className="body-widget-container pt-2 flex justify-center items-center gap-3">
           <img src={voteImg} alt="vote" className='translate-y-8 z-0' />
@@ -18,7 +18,7 @@ const widget = () => {
         </div>
       </div>
       <div className="widget-container-2 flex-1 flex justify-end items-end">
-        <img src={indiaMapImg} alt="bharath map" className='w-[75%]' />
+        <img src={indiaMapImg} alt="bharath map" className='object-contain w-full h-full' />
       </div>
       </div>
       <div className="live-mobile-container px-2 absolute bottom-0 py-1 z-10 bg-[#3E4355] w-full flex justify-between items-center">
