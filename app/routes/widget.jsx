62d619199ab9e0ee8bbace566a8af04907f914d9
bhaskar.guthula <bhaskar.guthula@aadhan.in>
2023-11-29 13:54:05 +0530

    widget ui widget font weight modified

--- app/routes/widget.jsx
+++ app/routes/widget.jsx
@@ -6,7 +6,7 @@ const widget = () => {
     <div className='bg-[#262C42] min-h-screen relative text-white font__chivo overflow-hidden'>
       <div className="widget-container h-screen pb-[32.5px] flex items-end overflow-hidden">
       <div className="widget-container-1 flex-1 p-[0.5rem]">
-        <div className="body-widget-container pt-2 flex justify-start items-center gap-3">
+        <div className="body-widget-container pt-2 font-medium flex justify-start items-center gap-3">
           <img src={voteImg} alt="vote" className='translate-y-8 z-0' />
           <div className='info-widget-container text-[1.2rem]'>
             <p>ASSEMBLY <br /> ELECTION</p>
